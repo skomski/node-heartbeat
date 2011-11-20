465d0b80ae5e72a671cde0fbd6588f300697cbdd
Karl Skomski <karl@skomski.com>
2011-11-20 21:48:35 +0000

    Second commit

--- README.md
+++ README.md
@@ -2,16 +2,27 @@
 
 [![Build Status](https://secure.travis-ci.org/skomski/node-heartbeat.png)](http://travis-ci.org/skomski/node-heartbeat)
 
-The minimal heartbeat library.
+Simple heartbeat management.
 
 ## Install
 
 ```
-npm install heartbeat
+  npm install heartbeat
 ```
 
 ## Usage
 
+```js
+var Heartbeat  = require('heartbeat');
+
+var timeout = 5000;
+var manager = new Heartbeat.Manager(timeout);
+var callback = function() {
+
+}
+var id = '123';
+manager.check(id, callback).start();
+```
 ## License
 
 This module is licensed under the MIT license.
--- test/test-Manager.js
+++ test/test-Manager.js
@@ -3,23 +3,26 @@ var sinon = require('sinon');
 
 var Heartbeat = require('../index');
 
-test('Create emitter', function (t) {
-  t.plan(1);
+test('Create emitter and delete emitter', function (t) {
+  t.plan(5);
 
   var clock = sinon.useFakeTimers();
   var manager = new Heartbeat.Manager(1000);
   var cb = function() {
-    t.ok(true);
+    t.ok(true, 'fire emitter callback once');
   };
 
-  manager.emitter('42', cb).start();
+  t.ok(manager.emitter('42', cb).start(), 'start emitter');
   clock.tick(1000);
-  manager.emitter('42').stop();
+  t.ok(manager.emitter('42').stop(), 'stop emitter');
   clock.tick(1000);
+
+  t.ok(manager.removeEmitter('42', 'remove emitter'));
+  t.deepEqual(manager._emitters, {}, 'emitters empty');
 });
 
-test('Create check', function (t) {
-  t.plan(1);
+test('Create check and delete check', function (t) {
+  t.plan(5);
 
   var clock = sinon.useFakeTimers();
   var manager = new Heartbeat.Manager(1000);
@@ -27,7 +30,12 @@ test('Create check', function (t) {
     t.ok(true);
   };
 
-  manager.check('42', cb).start();
+  t.ok(manager.check('42', cb).start());
   clock.tick(1000);
   clock.tick(1000);
+
+  t.ok(manager.check('42').stop());
+
+  t.ok(manager.removeCheck('42'));
+  t.deepEqual(manager._checks, {});
 });
